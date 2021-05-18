c8c5fd1e01c5a6a2f02a51cd583a938e4efe2260
ŞEYHMUS ASKAR <seyhmus.as@gmail.com>
2021-05-18 15:13:41 +0300

    sign out

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 import { LoginModel } from '../models/loginModel';
 import { SingleResponseModel } from '../models/singleResponseModel';
 import { TokenModel } from '../models/tokenModel';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Injectable({
 	providedIn: 'root'
@@ -10,17 +13,32 @@ import { TokenModel } from '../models/tokenModel';
 export class AuthService {
 
 	apiUrl = 'https://localhost:44380/api/auth/';
-	constructor(private httpClient: HttpClient) { }
+	constructor(
+		private httpClient: HttpClient,
+		private router: Router,
+		private toastrService: ToastrService
+	) { }
 
 	login(loginModel: LoginModel) {
-		return this.httpClient.post<SingleResponseModel<TokenModel>>(this.apiUrl+"login",loginModel)
+		return this.httpClient.post<SingleResponseModel<TokenModel>>(this.apiUrl + "login", loginModel)
 	}
-	isAuthenticated(){
-		if(localStorage.getItem("token")){
-		  return true;
+	isAuthenticated() {
+		if (localStorage.getItem("token")) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	signOut(): void {
+
+		if (localStorage.getItem('token')==null) {
+			this.toastrService.info("giriş olmadan çıkış nasıl olacak hele bi söyle")
 		}
 		else{
-		  return false;
+			localStorage.removeItem('token');
+			this.toastrService.success("You've been logged out");
+			this.router.navigate(['login']);
 		}
-	   }
+	}
 }
